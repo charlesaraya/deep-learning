4de7ff60cd3815588b95ee0b9735b2142a898e07
Charles Araya <charlesaraya@gmail.com>
2025-01-03 15:41:24 +0100

    doc: add docstrings to all methods
    Follows google-style docstring.

--- mlp.py
+++ mlp.py
@@ -3,30 +3,41 @@ import numpy as np
 class MLP(object):
     """A simple implementation of a Multi-Layer Perceptron (MLP) for basic machine learning tasks."""
 
-    def __init__(self, input_layer, hidden_layer, output_layer):
-        """Initializes the MLP model with specified layer sizes and learning rate."""
+    def __init__(self, input_layer: int, hidden_layer: list[int], output_layer: int):
+        """Initializes the MLP model with specified layer sizes.
+
+        Args:
+            input_layer (int): Number of neurons in the input layer. Corresponds to the number of features in the input data.
+            hidden_layer (list[int]): Number of neurons in the hidden layer. The list defines the architecture of the hidden layers.
+            output_layer (int): Number of neurons in the output layer. Corresponds to the number of output classes for classification.
+        """
         self.hidden_layer = hidden_layer
         self.nlayers = len(hidden_layer) + 1 # hidden layers + output layer
         
-        self.weights = []
-        self.biases = []
 
-        # Hidden layers
+        # Init hidden layers weights and bias
+        self.weights, self.biases = [], []
         prev_dim = input_layer
         for dim in self.hidden_layer:
             self.weights.append(np.random.randn(prev_dim, dim) * 0.1)
             self.biases.append(np.zeros((1, dim)))
             prev_dim = dim
 
-        # Output layer
+        # Init output layer and gradients w.r.t the weights and bias
         self.weights.append(np.random.randn(prev_dim, output_layer) * 0.1)
         self.biases.append(np.zeros((1, output_layer)))
-
         self.dW = [0] * self.nlayers
         self.db = [0] * self.nlayers
 
     def one_hot_encode(self, y_target):
-        """Returns the one-hot encoding of each label class"""
+        """Encodes each target label class into its one-hot format.
+
+        Args:
+            y_target (ndarray): Array of integers representing the target labels.
+
+        Returns:
+            ndarray: Array of float arrays representing the one-hot encoded vector of the target labels.
+        """
         nlabels = max(y_target) + 1
         y_encoded  = []
         for yi in y_target:
@@ -35,7 +46,14 @@ class MLP(object):
         return np.asarray(y_encoded)
 
     def forward(self, X):
-        """Forward pass through the network."""
+        """Performs the forward pass through the network.
+
+        Args:
+            X (ndarray): Input data. Each row corresponds to a sample and each column corresponds to a feature.
+
+        Returns:
+            ndarray: Output of the network. Each row corresponds to the predicted values for each sample, and each column corresponds to a target label.
+        """
         self.logits = []
 
         # Input layer
@@ -58,7 +76,14 @@ class MLP(object):
         return y_hat
 
     def backward(self, y_hat, y):
-        """Backward pass to compute gradients and update weights."""
+        """Performs the backward pass through the network.
+        
+        Computing the gradients of the loss w.r.t. the model parameters, and updates the weights and biases.
+
+        Args:
+            y_hat (ndarray): The predicted output from the forward pass.
+            y (ndarray): The true target labels for each training sample.
+        """
 
         # Gradient of the loss w.r.t. predictions
         grad_loss = y_hat  - y
@@ -79,7 +104,24 @@ class MLP(object):
             self.biases[i] -= self.learning_rate * self.db[i]
 
     def train(self, train_data, epochs, learning_rate):
-        """Train the MLP on provided training data."""
+        """Trains the MLP on the training data.
+        
+        Performs forward and backward passes at a given learning rate, and over a number of epochs.
+
+        Args:
+            train_data (tuple): A tuple containing the training data, where:
+                - X (ndarray): The input data.
+                - y (ndarray): The true target labels that exist in the classification task.
+            epochs (int): The number of times the model will iterate over the entire training dataset.
+            learning_rate (float): The learning rate used for gradient descent to update the model parameters.
+
+        Returns:
+            dict: Dictionary containing the following:
+            - 'weights' (list[ndarray]): Final weights of the model.
+            - 'bias' (list[ndarray]): Final biases of the model.
+            - 'training_accuracies' (list): Training accuracy values recorded at each epoch.
+            - 'training_losses' (list): Training loss values recorded at each epoch.
+        """
         training_accuracies, training_losses = [], []
         self.learning_rate = learning_rate
 
