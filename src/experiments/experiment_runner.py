3a08ba202fc9d21ee393508564a4c2adf1087ee5
Charles Araya <charlesaraya@gmail.com>
2025-01-14 22:43:16 +0100

    refactor: support new basemodel layering

--- src/experiments/experiment_runner.py
+++ src/experiments/experiment_runner.py
@@ -1,63 +1,133 @@
 import numpy as np
-from src.model.mlp import MLP
-from src.data.mnist_data import MNISTDatasetManager
 import json
 import os
+from math import ceil
+
+from model.basemodel import BaseModel
+from data.mnist_data import MNISTDatasetManager
+from data.encoders import OneHotEncoder, SmoothLabelEncoder
+from optimizers.schedulers import WarmUpScheduler, StepDecayScheduler, ExponentialDecayScheduler, CosineAnnealingScheduler
+
+from layers.denselayer import DenseLayer
+from layers.batchnorm import BatchNorm
+from layers.activations import Sigmoid, Tanh, ReLU, SoftMax
+from layers.regularizations import Dropout
+
+LAYERS = {
+    'dense': DenseLayer,
+    'sigmoid': Sigmoid,
+    'tanh': Tanh,
+    'relu': ReLU,
+    'softmax': SoftMax,
+    'batchnorm': BatchNorm,
+    'dropout': Dropout
+}
+
+SCHEDULERS = {
+    'warmup': WarmUpScheduler,
+    'step': StepDecayScheduler,
+    'exponential': ExponentialDecayScheduler,
+    'cosine': CosineAnnealingScheduler
+}
+
+ENCODERS = {
+    'onehot': OneHotEncoder,
+    'smoothlabel': SmoothLabelEncoder
+}
 
 class ExperimentRunner:
-    def __init__(self, model: MLP, datamanager: MNISTDatasetManager, config: dict):
-        self.model = model
-        self.datamanager = datamanager
+    def __init__(self, model: BaseModel, datamanager: MNISTDatasetManager, config: dict):
         self.config = config
+        # Init Data Manager
+        self.datamanager: MNISTDatasetManager = datamanager(
+            self.config['dataset']['batch_size'],
+            ENCODERS[self.config['dataset']['encoder']]()
+        )
+        # Load Datasets
+        self.datamanager.load_data(
+            self.config['dataset']['train_images_filepath'],
+            self.config['dataset']['train_labels_filepath'],
+            type = 'train'
+        )
+        self.datamanager.load_data(
+            self.config['dataset']['test_images_filepath'],
+            self.config['dataset']['test_labels_filepath'],
+            type = 'test'
+        )
+        # Prep Data
+        self.datamanager.prepdata(
+            type = 'train',
+            shuffle = self.config['dataset']['shuffle_train_set'],
+            validation_len = self.config['dataset']['validation_set_length']
+        )
+        self.test_data = self.datamanager.prepdata(
+            type = 'test',
+            shuffle = self.config['dataset']['shuffle_test_set']
+        )
+        # Scheduler
+        steps_per_epoch = ceil(self.datamanager.train_data[0].shape[0] / self.datamanager.batch_size)
+        step_size = ceil(steps_per_epoch*config['scheduler']['base']['step_ratio'])
+        basemodel = SCHEDULERS[config['scheduler']['base']['name']](
+            config['scheduler']['base']['learning_rate'],
+            step_size,
+            config['scheduler']['base']['decay_factor']
+        )
+        if config['scheduler']['main']['name'] == 'warmup':
+            steps_total = steps_per_epoch * config['epochs']
+            self.scheduler = SCHEDULERS['warmup'](
+                basemodel,
+                config['scheduler']['main']['learning_rate_start'],
+                config['scheduler']['main']['learning_rate'],
+                config['scheduler']['main']['warmup_ratio'] * steps_total
+            )
+        else:
+            self.scheduler = basemodel
+
+        # Init Model
+        self.model: BaseModel = model()
+        for layer in self.config['layers']:
+            match layer['name']:
+                case 'dense':
+                    self.model.add(
+                        LAYERS[layer['name']](
+                            layer['input'],
+                            layer['output'],
+                            layer['weight_init']
+                        )
+                    )
+                case 'batchnorm':
+                    self.model.add(
+                        LAYERS[layer['name']](
+                            layer['dim']
+                        )
+                    )
+                case 'dropout':
+                    self.model.add(
+                        LAYERS[layer['name']](
+                            layer['rate']
+                        )
+                    )
+                case _:
+                    self.model.add(LAYERS[layer['name']]())
 
     def run(self) -> None:
         """Runs an experiment for a given configuration."""
-        # Init Data Manager
-        datamanager = self.datamanager(self.config['batch_size'])
-        # Load Datasets
-        datamanager.load_data(
-            self.config['train_images_filepath'],
-            self.config['train_labels_filepath'],
-            type = 'train'
-            )
-        datamanager.load_data(
-            self.config['test_images_filepath'],
-            self.config['test_labels_filepath'],
-            type = 'test'
-            )
-        # Prep Data
-        datamanager.prepdata(
-            type = 'train',
-            shuffle = self.config['shuffle_train_set'],
-            validation_len = self.config['validation_set_length']
-        )
-        test_data = datamanager.prepdata(
-            type = 'test',
-            shuffle = self.config['shuffle_test_set']
-        )
-
-        # Init Model
-        model = self.model(
-            self.config['input_layer'],
-            self.config['hidden_layers'],
-            self.config['output_layer']
-        )
         # Train Model
-        results = model.train(
-            datamanager,
-            self.config['epochs'],
-            self.config['learning_rate']
+        results = self.model.train(
+            self.datamanager,
+            self.scheduler,
+            self.config['epochs']
         )
         # Evaluate
-        test_accuracy = self.evaluate(model, test_data)
+        test_accuracy = self.evaluate(self.model, self.test_data)
 
         # Log Results
         self.log_results(results, test_accuracy)
 
-    def evaluate(self, model, test_data):
+    def evaluate(self, model: BaseModel, test_data: np.ndarray):
         """Evaluates the model on the test dataset."""
         # Inference
-        test_probabilities = model.forward(test_data[0])
+        test_probabilities = model.forward(test_data[0], is_training=False)
         test_predictions = np.argmax(test_probabilities, axis=1)
         # Calculate Accuracy
         test_accuracy = np.mean(test_predictions == test_data[1])
@@ -75,9 +145,10 @@ class ExperimentRunner:
 
     def log_results(self, train_results, test_accuracy):
         """Logs the results of the experiment."""
-        model_name = self._create_model_name()
-        print(f"\n{model_name}, Epochs: {self.config['epochs']}, Batch size: {self.config['batch_size']}, " +
-                f"Learning rate: {self.config['learning_rate']} \
+        #model_name = self._create_model_name()
+        model_name = self.model.__str__()
+        print(f"\n{model_name}, Epochs: {self.config['epochs']}, Batch size: {self.config['dataset']['batch_size']}, " +
+                f"Learning rate: {self.config['scheduler']['base']['learning_rate']} \
                 \n{"─" * 15} Loss {"─" * 20} \
                 \nTraining Loss:\t{train_results['training_losses'][-1]:.3} \
                 \nValid Loss:\t{train_results['validation_losses'][-1]:.3} \
@@ -93,7 +164,7 @@ class ExperimentRunner:
         if not os.path.exists(experiment_filepath):
             os.makedirs(experiment_filepath)
 
-        experiment_params = f"_e{self.config['epochs']}_b{self.config['batch_size']}_lr{self.config['learning_rate']:.2}"
+        experiment_params = f"_e{self.config['epochs']}_b{self.config['dataset']['batch_size']}_lr{self.config['scheduler']['base']['learning_rate']:.2}"
         experiment_name = 'experiment_' + experiment_params + '.json'
         experiment_filepath = os.path.join(
             experiment_filepath,
@@ -102,5 +173,7 @@ class ExperimentRunner:
         with open(experiment_filepath, 'w') as f:
             json.dump({
                 'training_losses': train_results['training_losses'],
-                'training_accuracies': train_results['training_accuracies']
+                'training_accuracies': train_results['training_accuracies'],
+                'validation_losses': train_results['validation_losses'],
+                'validation_accuracies': train_results['validation_accuracies']
                 }, f)
