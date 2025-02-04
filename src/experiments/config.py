18f434dd74918845bb236884243ef5cb0e04414c
Charles Araya <charlesaraya@gmail.com>
2025-02-04 13:35:13 +0100

    chore: remove momentum from default config

--- src/experiments/config.py
+++ src/experiments/config.py
@@ -52,12 +52,13 @@ def get_cfg_defaults():
     _C.model = CfgNode()
     _C.model.name = None
     _C.model.show_summary = False
+    _C.model.verbose = False
     _C.model.batch_eval = 32
     _C.model.optimizer = 'sgd'
     _C.model.optimizer = CfgNode()
     _C.model.optimizer.name = 'sgd'
     _C.model.optimizer.params = CfgNode()
-    _C.model.optimizer.params.momentum = 0.8
+    _C.model.optimizer.params.momentum = 0.0
 
     # Checkpoint configuration
     _C.checkpoint = CfgNode()
